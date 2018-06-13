0300464a45faba657be4a09201c1edb627f2b4f3
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-06-13 09:54:56 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
-describe('shout(string)', function() {
+describe('shout(string)', function shout(string) {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
