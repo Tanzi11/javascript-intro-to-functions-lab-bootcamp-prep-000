e0fcdbcfc05abfe8e95a16337519b8aca962931c
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-06-13 11:19:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ function logWhisper(string) {
   return console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) return "I can't hear you!";
-  else if (string===string.toUpperCase()) return "YES, INDEED!";
-  else if (string==="I love you, Grandma.") return "I love you, too";
+  if (string === string.toLowerCase()) 
+  return "I can't hear you!";
+  else if (string===string.toUpperCase()) 
+  return "YES, INDEED!";
+  else if (string==="I love you, Grandma.") 
+  return "I love you, too";
 }
