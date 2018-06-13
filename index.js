917f6fb8e868a864a51d1b740c3140b16c6791d1
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-06-13 10:59:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   return console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string===string.toLowerCase)
+  if (string===string.toLowerCase())
   
 }
