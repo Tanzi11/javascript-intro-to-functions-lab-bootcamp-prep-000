07e05e0cf0e778a3a8178e6de74da31a309e730e
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-06-13 10:00:19 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,5 @@
-describe('shout(string)', function shout(string) {
-  it('receives one argument and returns it in all caps', function shout (string) {
+describe('shout(string)', function () {
+  it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
