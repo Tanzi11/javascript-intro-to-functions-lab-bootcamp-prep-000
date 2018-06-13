943813ba364d6687aeec348a30aa289dff2a1899
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-06-13 11:02:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) return "I can't hear you!";
-  else if (string === "I love you, Grandma."
+  else if (string === "I love you, Grandma.")
+  
 }
